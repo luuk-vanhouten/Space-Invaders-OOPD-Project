cd19943199d4d79f85a3db11f172c9c21eb6e2cb
Luuk van Houten <l.vanhouten3@student.han.nl>
2021-10-11 14:25:43 +0200

    Added automated movement issue #4

--- src/main/java/com/github/hanyaeger/tutorial/entities/endboss/EndBoss.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/endboss/EndBoss.java
@@ -4,17 +4,14 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.DynamicCompositeEntity;
 import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
 import com.github.hanyaeger.api.scenes.SceneBorder;
-import com.github.hanyaeger.api.userinput.KeyListener;
-import javafx.scene.input.KeyCode;
 
-import java.util.Set;
-
-public class EndBoss extends DynamicCompositeEntity implements KeyListener, SceneBorderTouchingWatcher{
+public class EndBoss extends DynamicCompositeEntity implements SceneBorderTouchingWatcher{
 
     private EndBossSprite endBossSprite;
 
     public EndBoss(Coordinate2D initialLocation) {
         super(initialLocation);
+        setMotion(3, 270d);
     }
 
     @Override
@@ -22,38 +19,6 @@ public class EndBoss extends DynamicCompositeEntity implements KeyListener, Scen
         endBossSprite = new EndBossSprite(new Coordinate2D(0, 0));
         addEntity(endBossSprite);
         addEntity(new EndBossHitBox(new Coordinate2D(0, 0)));
-    }
-
-
-    @Override
-    public void onPressedKeysChange(Set<KeyCode> pressedKeys){
-        if(pressedKeys.contains(KeyCode.LEFT)){
-            moveLeft();
-        } else if(pressedKeys.contains(KeyCode.RIGHT)){
-            moveRight();
-        } else if(pressedKeys.contains(KeyCode.UP)){
-            moveForward();
-        } else if(pressedKeys.contains(KeyCode.DOWN)){
-            moveBackward();
-        } else if(pressedKeys.isEmpty()){
-            setSpeed(0);
-        }
-    }
-
-    private void moveLeft(){
-        setMotion(5,270d);
-    }
-
-    private void moveRight(){
-        setMotion(5,90d);
-    }
-
-    private void moveForward(){
-        setMotion(5,180d);
-    }
-
-    private void moveBackward(){
-        setMotion(5,0d);
     }
 
     @Override
@@ -69,9 +34,11 @@ public class EndBoss extends DynamicCompositeEntity implements KeyListener, Scen
                 break;
             case LEFT:
                 setAnchorLocationX(1);
+                setMotion(3, 90d);
                 break;
             case RIGHT:
                 setAnchorLocationX(getSceneWidth() - getWidth() - 1);
+                setMotion(3, 270d);
             default:
                 break;
         }
